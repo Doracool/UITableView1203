d1d7618fc813d0d23be2ae6b6dfb57e8fa960aeb
Doracool <751379858@qq.com>
2015-12-03 09:40:04 +0800

    初学Swift，设置变量和输出

---
+++ Swift.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("Hello,Word!");
+
+
+var aNumber = 2;
+var aOtherNumber = 5;
+
+var Number = aNumber + aOtherNumber;
+
+print("the number is equal\(Number)");
