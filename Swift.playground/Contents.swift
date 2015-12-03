1e3f2d77b46dbc2ed9a298112e84e3b0feac59db
Doracool <751379858@qq.com>
2015-12-03 14:42:06 +0800

    whlie语句

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -13,3 +13,12 @@ var aOtherNumber = 5;
 var Number = aNumber + aOtherNumber;
 
 print("the number is equal\(Number)");
+
+
+var i = 0
+
+while (i < 10)
+{
+    print("HELLO,WORLD")
+    i = i + 1
+}
